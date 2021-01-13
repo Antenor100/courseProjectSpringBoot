25cde3453ee9915f85c34a47075a9ceb4b0a3208
Antenor Manoel <antenor_manuel2010@hotmail.com>
2021-01-13 00:26:01 -0300

    OrderDTO

--- src/main/java/com/iftm/course/resources/OrderResource.java
+++ src/main/java/com/iftm/course/resources/OrderResource.java
@@ -1,6 +1,6 @@
 package com.iftm.course.resources;
 
-import com.iftm.course.entities.Order;
+import com.iftm.course.dto.OrderDTO;
 import com.iftm.course.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,15 +19,15 @@ public class OrderResource {
 	private OrderService service;
 
 	@GetMapping
-	public ResponseEntity<List<Order>> findAll() {	
-		List<Order> list = service.findAll();
+	public ResponseEntity<List<OrderDTO>> findAll() {	
+		List<OrderDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Order> findById(@PathVariable Long id) {
-		Order obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);
+	public ResponseEntity<OrderDTO> findById(@PathVariable Long id) {
+		OrderDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
 	}
 	
 }
